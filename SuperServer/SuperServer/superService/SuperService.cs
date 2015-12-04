22bdd178710aa399a64bb3e953306325702f6ad9
Land <ludidilu@163.com>
2015-12-05 03:05:31 +0800

    修改SuperService，防止process和call冲突导致线程闲置。

--- SuperServer/SuperServer/superService/SuperService.cs
+++ SuperServer/SuperServer/superService/SuperService.cs
@@ -10,39 +10,90 @@ namespace SuperServer.superService
         private readonly SuperLocker processLocker = new SuperLocker();
         private readonly SuperLocker callLocker = new SuperLocker();
 
-        private bool inProcessQueue = false;
+        private bool inProcessQueue = false;//判断是否已经召唤线程去执行process队列了  使用这个属性一定要持有processLocker的锁
 
-        private List<Delegate> actionList = new List<Delegate>();
+        private int callNum = 0;//记录call的等待数量  使用这个属性一定要持有processLocker的锁
+
+        private List<Delegate> processActionList = new List<Delegate>();
 
         public void Process<T>(Action<T> _action) where T : SuperService
         {
+            bool needCallThread = false;
+
             lock (processLocker)
             {
-                actionList.Add(_action);
+                processActionList.Add(_action);
 
-                if (!inProcessQueue)
+                if (!inProcessQueue && callNum == 0)
                 {
                     inProcessQueue = true;
 
-                    ThreadPool.QueueUserWorkItem(StartProcess<T>);
+                    needCallThread = true;
                 }
+            }
+
+            if (needCallThread)
+            {
+                ThreadPool.QueueUserWorkItem(StartProcess<T>);
+            }
+        }
+
+        private void BeginCall()
+        {
+            lock (processLocker)
+            {
+                callNum++;
+            }
+        }
+
+        private void EndCall<T>() where T : SuperService
+        {
+            bool needCallThread = false;
+
+            lock (processLocker)
+            {
+                callNum--;
+
+                if (!inProcessQueue && callNum == 0 && processActionList.Count > 0)
+                {
+                    inProcessQueue = true;
+
+                    needCallThread = true;
+                }
+            }
+
+            if (needCallThread)
+            {
+                ThreadPool.QueueUserWorkItem(StartProcess<T>);
             }
         }
 
         public void Call<T>(Action<T> _action) where T : SuperService
         {
+            BeginCall();
+
             lock (callLocker)
             {
                 _action(this as T);
             }
+
+            EndCall<T>();
         }
 
         public R Call<T,R>(Func<T,R> _action) where T : SuperService
         {
+            R result;
+
+            BeginCall();
+
             lock (callLocker)
             {
-                return _action(this as T);
+                result = _action(this as T);
             }
+
+            EndCall<T>();
+
+            return result;
         }
 
         private void StartProcess<T>(object _param) where T : SuperService
@@ -53,16 +104,16 @@ namespace SuperServer.superService
 
                 lock (processLocker)
                 {
-                    if (actionList.Count == 0)
+                    if (processActionList.Count == 0 || callNum > 0)
                     {
                         inProcessQueue = false;
 
                         return;
                     }
 
-                    action = actionList[0] as Action<T>;
+                    action = processActionList[0] as Action<T>;
 
-                    actionList.RemoveAt(0);
+                    processActionList.RemoveAt(0);
                 }
 
                 lock (callLocker)
